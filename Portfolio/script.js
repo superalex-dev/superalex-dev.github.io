82aec67b48aa7f18a9f98cf5b8ecedd7300e2b74
Alexander Boev <mr.naidobrixjr@gmail.com>
2022-10-20 13:12:58 +0300

    Initial commit

---
+++ Portfolio/script.js
@@ -0,0 +1,3 @@
+window.onload = function() { document.body.classList.remove('is-preload'); }
+			window.ontouchmove = function() { return false; }
+			window.onorientationchange = function() { document.body.scrollTop = 0; }
